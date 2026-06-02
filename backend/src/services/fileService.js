f4d4fe46244b715982f570ca6134051737967663
Rayana <khshva.77@gmail.com>
2026-06-02 16:16:58 +0300

    Create fileService.js

---
+++ backend/src/services/fileService.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const readData = (filePath) => {
+  const data = fs.readFileSync(filePath, 'utf-8');
+  return JSON.parse(data);
+};
+
+const writeData = (filePath, data) => {
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+};
+
+module.exports = {
+  readData,
+  writeData
+};
