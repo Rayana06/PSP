948efdd872ecb57c56ccd522093dd8ba9760fb0a
Rayana <khshva.77@gmail.com>
2026-06-02 14:24:00 +0300

    Delete frontend/components directory

--- frontend/components/back-button/back-button.js
+++
@@ -1,8 +0,0 @@
-export class BackButtonComponent{
-constructor(parent){this.parent=parent;}
-render(listener){
-this.parent.insertAdjacentHTML("beforeend",
-'<button class="btn btn-secondary back-btn">Назад</button>');
-this.parent.querySelector("button").addEventListener("click",listener);
-}
-}
